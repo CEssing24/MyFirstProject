773b785966560b44240384add368571deb504973
CEssing24 <111998757+CEssing24@users.noreply.github.com>
2022-08-29 10:49:36 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,16 @@
+/*
+Carter Essing
+C++ fall 2022
+Due : Aug 31, 2022
+Lab 1 Introduction to C++ Programing
+This lab is a demo of my ability to work with an IDE and GitHub
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << "Dark Green";
+
+	return 0;
+}
